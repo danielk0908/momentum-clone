5a94135b9a67bd26ffa42b526b6ca0ac679be3db
danielk0908 <jaehyung.kim.93@gmail.com>
2021-05-26 20:45:29 -0400

    css in progress

--- todo.js
+++ todo.js
@@ -26,7 +26,7 @@ function paintToDo(text) {
   const delBtn = document.createElement("button");
   const span = document.createElement("span");
   const newId = toDos.length + 1;
-  delBtn.innerText = "X";
+  delBtn.innerHTML = '<i class="far fa-trash-alt"></i>';
   delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(span);
